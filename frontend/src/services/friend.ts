12aca5a79bb97686a4268aba78554a73e74a9951
Tiep Minh Nguyen <tiep2403nbk@gmail.com>
2026-01-27 23:03:49 -0500

    Add create functions for friend service (postFriendRequest and postFriend)

--- frontend/src/services/friend.ts
+++ frontend/src/services/friend.ts
@@ -44,7 +44,6 @@ export async function getFriendRequests(userId: string) {
     .select(
       `
         sender_id,
-        is_pending,
         user!friend_request_sender_id_fkey (
           username,
           image_url
@@ -58,6 +57,54 @@ export async function getFriendRequests(userId: string) {
   return data.map((row) => ({
     ...row.user,
     user_id: row.sender_id,
-    is_pending: row.is_pending,
   }));
+}
+
+export async function postFriend(userId: string, senderId: string) {
+  const { error } = await supabase.rpc("accept_friend_request", {
+    p_user_id: userId,
+    p_sender_id: senderId,
+  });
+
+  if (error) throw error;
+}
+
+export async function postFriendRequest(userId: string, receiverId: string) {
+  // Check if userId = receiverId
+  if (userId === receiverId) throw new Error("Cannot send request to yourself");
+
+  // Check if they're already friends
+  const { data: friendData, error: friendError } = await supabase
+    .from("friend")
+    .select("*")
+    .or(
+      `and(user1_id.eq.${userId},user2_id.eq.${receiverId}),and(user1_id.eq.${receiverId},user2_id.eq.${userId})`,
+    );
+
+  if (friendError) throw friendError;
+
+  if (friendData && friendData.length > 0)
+    throw new Error("Users are already friends");
+
+  // Check if a pending friend request already exists
+  const { data: requestData, error: requestError } = await supabase
+    .from("friend_request")
+    .select("*")
+    .or(
+      `and(sender_id.eq.${userId},receiver_id.eq.${receiverId}),and(sender_id.eq.${receiverId},receiver_id.eq.${userId})`,
+    );
+
+  if (requestError) throw requestError;
+
+  if (requestData && requestData.length > 0) {
+    throw new Error("A friend request already exists.");
+  }
+
+  // Create a new friend request
+  const { error } = await supabase.from("friend_request").insert({
+    sender_id: userId,
+    receiver_id: receiverId,
+  });
+
+  if (error) throw error;
 }
