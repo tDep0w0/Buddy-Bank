9283f3484e2db670b56dec068a4fc5826650c3b7
nhanpham-ptn <nhanpham.ptn2110@gmail.com>
2026-02-22 01:31:13 +0700

    Add search_users router to main application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api import receipts
+from app.api import search_users
 
 app = FastAPI()
 
 app.include_router(receipts.router, prefix="/api/receipts", tags=["receipts"])
+app.include_router(search_users.router, prefix ="/api/search_users", tags=["search_users"])
