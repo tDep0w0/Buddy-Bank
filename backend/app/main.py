b16d23007d10c73ceedf54a5132e3027f43192e7
nhanpham-ptn <nhanpham.ptn2110@gmail.com>
2026-02-22 04:51:49 +0700

    Add import for users API in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from app.api import receipts
-from app.api import search_users
+from app.api import users
 
 app = FastAPI()
 
 app.include_router(receipts.router, prefix="/api/receipts", tags=["receipts"])
 app.include_router(search_users.router, prefix ="/api/search_users", tags=["search_users"])
+
